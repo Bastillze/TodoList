c1979a4201c3cf9017575860dd487bfefa0ce479
Lance <contact@lefside.com>
2023-03-03 20:22:13 -0500

    Fifth Commit

---
+++ src/Checklist.js
@@ -0,0 +1,18 @@
+class checkList{
+
+    constructor(checks){
+
+        this.check1;
+        this.check2;
+        this.check3;
+    }
+
+    get checks(){
+        return this.checks
+    }
+
+    set checks(checker){
+
+        alert("Checklist");
+    }
+}
---
+++ src/ToDoListPriority.js
@@ -0,0 +1,20 @@
+let priority= (function () {
+   'use strict'
+
+   this.priority=()=>console.log('Priority Name');
+
+   function priority(){
+    return this.priority;
+   }
+ 
+   function newPriority(){
+   
+    if(priority !== done){
+        alert("This Priority isn't completed");
+        return;
+    }  
+   }
+
+   this.priority = value;
+
+ })();
