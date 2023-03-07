0bf50bb854c8ba7bc595ccd7f4ba7d7455004bae
roozt <contact@lefside.com>
2023-03-07 06:04:43 -0500

    Sixth Commit Added Lodash

---
+++ src/index.js
@@ -0,0 +1,10 @@
+let toDoList = (function () {
+   'use strict'
+
+   this.thingsIveDone=()=>console.log("Things I've Done");
+   this.thingsToDo=()=>console.log("Things to Do:");
+   console.log("What's new to do Today?")
+
+
+
+ })();
