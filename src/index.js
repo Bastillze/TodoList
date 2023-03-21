59feb3de7bf57278af1cd4706c5d1dfb67b25ac9
Lance <contact@lefside.com>
2023-03-21 18:13:42 -0400

    Added RequireJs

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
-define("toDoList",
-[ToDoListDescription,Checklist,],
-(function(){
-    'use strict'
-    let toDoList= document.getElementById("actualToDoList");
+define("toDoList", [ToDoList, Description, Checklist], function () {
+  "use strict";
+  let toDoList = document.getElementById("actualToDoList");
 
-    this.thingsDone=()=> console.log("What I've Done");
-    this.thingsToDo=()=>console.log("What to do today?");
+  this.thingsDone = () => console.log("What I've Done");
+  this.thingsToDo = () => console.log("What to do today?");
 
-
-   function toDo(){
-    if(thing != thingsDone){
-        thing.push(thingsToDo);
+  function toDo() {
+    if (thing != thingsDone) {
+      thing.push(thingsToDo);
     }
-   }
+  }
 
-    toDo = prompt("What do you want to do?");
-   if(thing = String){
+  let toDo = prompt("What do you want to do?");
+  if ((thing = String)) {
     toDo.push(newThing);
-   }
+  }
 
-   document.appendChild("actualToDoList");
+  document.appendChild("actualToDoList");
+})();
 
-})
-
-)();
+requirejs.config({
+  baseUrl: "src",
+  paths: {
+    description: "ToDoListDescription.js",
+    duedate: "ToDoListDueDate.js",
+    notes: "ToDoListNotes.js",
+    priority: "ToDoListPriorit.js",
+  },
+});
