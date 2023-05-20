066a447b10121d63465a64e1ce77906121fa1257
Bastillze <36552461+Bastillze@users.noreply.github.com>
2023-05-20 13:14:38 -0400

    Updated

--- src/index.js
+++ src/index.js
@@ -1,115 +1,116 @@
-define(function(require) {
-  class Model {
-    constructor() {
-      this.todos = [
-        { id: 1, text: "Run a marathon", complete: false },
-        {
-          id: 2,
-          text: "join a fighting game tournament",
-          complete: false,
-        },
-      ];
-    }
-      addTodo(todoText) {
-        const todo = {
-          id:
-            this.todos.length > 0
-              ? this.todos[this.todos.length - 1].id + 1
-              : 1,
-          text: todoText,
-          complete: false,
-        }
+const itemsArray = localStorage.getItem("items") ? JSON.parse(localStorage.getItem("items")) :
+[]
 
-        this.todos.push(todo);
+console.log(itemsArray)
 
-      }
+document.querySelector("#enter").addEventListener("click", () => {
+  const item = document.querySelector("#item")
+  createItem(item)
+})
 
-      
+function displayItems() {
 
-      editTodo(id, updatedText) {
-        this.todos = this.todos.map((todo) =>
-          todo.id === id
-            ? { id: todo.id, text: updatedText, complete: todo.complete }
-            : todo
-        );
-      }
-
-      deleteTodo(id) {
-        this.todos = this.todos.filter((todo) => todo.id !== id);
-      }
-
-      toggleTodo(id) {
-        this.todos = this.todos.map((todo) =>
-          todo.id === id
-            ? { id: todo.id, text: todo.text, complete: !todo.complete }
-            : todo
-        );
-      }
-    }
-  
-
-  class View {
-    constructor() {
-
-    this.app = this.getElement('#root')
-
-    this.title = this.createElement('h3')
-    this.title.textContent = 'Todos'
-
-    this.form = this.createElement('form')
-
-    this.input = this.createElement('input')
-    this.input.type = 'text'
-    this.input.placeholder = 'Add todo'
-    this.input.name = 'todo'
-
-    this.submitButton = this.createElement('button')
-    this.submitButton.textContent = 'Submit'
-
-    this.todoList = this.createElement('ul', 'todo-list')
-
-    this.form.append(this.input, this.submitButton)
-
-    this.app.append(this.title, this.form, this.todoList)
-
-    }
-
-    createElement(tag, className) {
-      const element = document.createElement(tag);
-      if (className) element.classList.add(className);
-
-      return element;
-    }
-
-    getElement(selector) {
-      const element = document.querySelector(selector);
-
-      return element;
-    }
+  let items = ""
+  for(let i = 0; i < itemsArray.length; i++){
+    items += `
+    <div class="item">
+      <div class="input-controller">
+      <textarea disabled>${itemsArray[i]}</textarea>
+      <div class="edit-controller">
+        <i class="fa-solid fa-check deleteBtn"></i>
+        <i class="fa-solid fa-pen-to-square editBtn"></i>
+      </div>
+      </div>
+      <div class="update-controller">
+         <button class="saveBtn">Save</button>
+         <button class="cancelBt">Cancel</button>
+      </div>
+    `
+    
   }
 
-  class Controller {
-    constructor(model, view) {
-      this.model = model;
-      this.view = view;
-    }
-  }
+  document.querySelector(".to-do-list").innerHTML = items
+  activateDeleteListeners()
+  activateEditListeners()
+  activateSaveListeners()
+  activateCancelListeners()
+}
 
-  const app = new Controller(new Model(), new View());
+function activateDeleteListeners() {
+  let deleteBtn = document.querySelectorAll(".deleteBtn")
+  deleteBtn.forEach((db, i) => {
+    db.addEventListener("click", () => { deleteItem(i) })
+  })
+}
 
-  
-});
 
-requirejs.config({
-  baseUrl: "src/index",
-  paths: {
-   
-    index: '../index'
+function activateEditListeners() {
+  const editBtn = document.querySelectorAll(".editBtn")
+  const updateController = document.querySelectorAll(".update-controller")
+  const inputs = document.querySelectorAll(".input-controller textarea")
+  editBtn.forEach((eb, i) => {
+    eb.addEventListener("click", () => {
+      updateController[i].getElementsByClassName.display = "block"
+      inputs[i].disabled = false
+    })
+  })
+}
 
-  },
-});
+function activateSaveListeners() {
+  const saveBtn = document.querySelectorAll(".saveBtn")
+  const inputs = document.querySelectorAll(".input-controller textarea")
+  saveBtn.forEach((sb, i) => {
+    sb.addEventListener("click", () => {
+      updateItem(inputs[i].value, i)
+    })
+  })
+}
 
-require(["index"], function (require) {
+function activateCancelListeners() {
+  const cancelBtn = document.querySelectorAll(".cancelBtn")
+  const updateController = document.querySelectorAll(".update-controller")
+  const inputs = document.querySelectorAll(".input-controller textarea")
+  cancelBtn.forEach((cb, i) => {
+    cb.addEventListener("click", () => {
+      updateController[i].getElementsByClassName.display = "none"
+      inputs[i].disabled = true
+    })
+  })
+}
 
- 
-});
+
+
+function updateItem(text, i) {
+  itemsArray[i] = text
+  localStorage.setItem("items", JSON.stringify(itemsArray))
+  location.reload()
+}
+
+
+
+function deleteItem(i){
+  itemsArray.splice(i, 1)
+  localStorage.setItem("items", JSON.stringify(itemsArray))
+  location.reload()
+}
+
+
+function createItem(item){
+  itemsArray.push(item.value)
+  localStorage.setItem("items", JSON.stringify(itemsArray))
+  location.reload()
+}
+
+
+
+function displayDate(){
+  let date = new Date()
+date = date.toString().split(" ")
+document.querySelector("#date").innerHTML = date[1] + " " + date[2] + " " + date[3]
+}
+
+window.onload = function(){
+
+  displayDate()
+  displayItems()
+}
